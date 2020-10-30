116820f0dd856bc25686d453619ca274168e8788
irving <irving@idelacruz.com>
2020-10-30 21:30:56 +0800

    new

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello there Irving! WHOA test'));
+app.get("/", (req, res) => {
+    res.send("Welcome");
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
